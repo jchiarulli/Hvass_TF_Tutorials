758857d1812f33638c8f1be0b4e2449bb20086e2
jason <jason@Champ.jason>
2019-05-17 13:27:35 -0400

    Updated

---
+++ Convolutional_Neural_Network.py
@@ -0,0 +1,8 @@
+import matplotlib.pyplot as plt
+import tensorflow as tf
+import numpy as np
+from sklearn.metrics import confusion_matrix
+import time
+from datetime import timedelta
+import math
+
